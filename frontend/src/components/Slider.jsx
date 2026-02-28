48a21f061248d1d8224b65570b4d8ba0f173d555
CarlosSalazar34 <carloseliassalazaryunes@gmail.com>
2026-02-28 19:58:49 -0400

    add changes in the title

--- frontend/src/components/Slider.jsx
+++ frontend/src/components/Slider.jsx
@@ -1,7 +1,7 @@
 export default function Slider(){
     return (
-        <section className="min-h-screen ">
-            <h1 className="text-2xl font-bold text-center">La nueva gama de iphone Esta aqui</h1>
+        <section className="min-h-screen flex items-center justify-start ml-10">
+            <h1 className="text-4xl font-extrabold sm:text-6xl leading-tight max-w-1xl">La nueva gama de iphone Esta aqui</h1>
         </section>
     )
 }
